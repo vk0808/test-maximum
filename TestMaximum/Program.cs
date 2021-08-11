74fd803752efb37b4965dbbb3ae6dee2cd63e8c5
vk0808 <vk4765712@gmail.com>
2021-08-11 23:46:35 +0530

    feat: added maxFloat class

--- TestMaximum/Program.cs
+++ TestMaximum/Program.cs
@@ -11,18 +11,18 @@ namespace TestMaximum
 
 
             // Passing input with max number at first place
-            int outputInt1 = CheckMaximumInt.MaxIntNumber(34, 23, 12);
+            double outputInt1 = CheckMaximumFloat.MaxFloatNumber(3.4, 2.3, 1.2);
 
             // Passing input with max number at second place
-            int outputInt2 = CheckMaximumInt.MaxIntNumber(129, 489, 19);
+            double outputInt2 = CheckMaximumFloat.MaxFloatNumber(12.9, 48.9, 1.9);
 
             // Passing input with max number at third place
-            int outputInt3 = CheckMaximumInt.MaxIntNumber(64, 102, 108);
+            double outputInt3 = CheckMaximumFloat.MaxFloatNumber(6.4, 12.0, 18.0);
 
             // Print output
-            Console.WriteLine($"Max of (34, 23, 12) is {outputInt1}");
-            Console.WriteLine($"Max of (129, 489, 19) is {outputInt2}");
-            Console.WriteLine($"Max of (64, 102, 108) is {outputInt3}");
+            Console.WriteLine($"Max of (3.4, 2.3, 1.2) is {outputInt1}");
+            Console.WriteLine($"Max of (12.9, 48.9, 1.9) is {outputInt2}");
+            Console.WriteLine($"Max of (6.4, 12.0, 18.0) is {outputInt3}");
         }
     }
 }
