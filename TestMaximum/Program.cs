bd5e5cacd1d0a1c51a011bb8a60a549eaf83ef49
vk0808 <vk4765712@gmail.com>
2021-08-11 23:32:09 +0530

    test: second tc

--- TestMaximum/Program.cs
+++ TestMaximum/Program.cs
@@ -13,9 +13,12 @@ namespace TestMaximum
             // Passing input with max number at first place
             int outputInt1 = CheckMaximumInt.MaxIntNumber(34, 23, 12);
 
+            // Passing input with max number at second place
+            int outputInt2 = CheckMaximumInt.MaxIntNumber(129, 489, 19);
 
             // Print output
-            Console.WriteLine(outputInt1);
+            Console.WriteLine($"Max of (34, 23, 12) is {outputInt1}");
+            Console.WriteLine($"Max of (129, 489, 19) is {outputInt2}");
         }
     }
 }
