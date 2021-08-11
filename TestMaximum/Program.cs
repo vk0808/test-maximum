c6675421bb65e5e0d9d08cdf0f2a1005c118e3ce
vk0808 <vk4765712@gmail.com>
2021-08-11 18:55:22 +0530

    feat: initial commit

---
+++ TestMaximum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Test Maximum of 3 Variables Program\n");
+        }
+    }
+}
