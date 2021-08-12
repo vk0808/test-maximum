594ff0093cac3e88bef949be8a483b05d6b960b9
vk0808 <vk4765712@gmail.com>
2021-08-12 14:53:29 +0530

    feat: extend generic method

---
+++ TestMaximum/GenericMaxLarge.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMaximum
+{
+    // Generic class
+    public class GenericMaxLarge<T> where T : IComparable
+    {
+        // Instance variables
+        public T[] value;
+
+        // Constructor
+        public GenericMaxLarge(T[] value)
+        {
+            this.value = value;
+        }
+
+        // Generic method to sort the array
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+
+        // Generic method to get max value
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[^1];
+        }
+
+        // Generic method to call MaxValue method
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+
+        public string welcome()
+        {
+            return "Welcome to Test Maximum Program";
+        }
+    }
+}
--- TestMaximum/Program.cs
+++ TestMaximum/Program.cs
@@ -2,34 +2,24 @@
 
 namespace TestMaximum
 {
-    class Program 
+    class Program
     {
 
         static void Main(string[] args)
         {
-            // Display welcome message
-            Console.WriteLine(CheckMaximumInt.welcome());
-
+            int[] array = { 34, 23, 12, 36 };
 
             // Passing integer input with max number at first place
-            GenericMax<int> genInt = new GenericMax<int>(34, 23, 12);
-            int outputInt1 = genInt.MaxMethod();
+            GenericMaxLarge<int> genInt = new GenericMaxLarge<int>(array);
+            int outputInt = genInt.MaxMethod();
 
 
-            // Passing double input with max number at second place
-            GenericMax<double> genFloat = new GenericMax<double>(12.9, 48.9, 1.9);
-            double outputInt2 = genFloat.MaxMethod();
-
-
-            // Passing string input with max number at third place
-            GenericMax<string> genString = new GenericMax<string>("apple", "banana", "peach");
-            string outputInt3 = genString.MaxMethod();
+            // Display welcome message
+            Console.WriteLine(genInt.welcome());
 
 
             // Print output
-            Console.WriteLine($"Max of 3 int (34, 23, 12) is {outputInt1}");
-            Console.WriteLine($"Max of 3 double (12.9, 48.9, 1.9) is {outputInt2}");
-            Console.WriteLine($"Max of 3 string (apple, banana, peach ) is {outputInt3}");
+            Console.WriteLine($"Max value is {outputInt}");
         }
     }
 }
